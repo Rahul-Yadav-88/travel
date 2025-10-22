bdc31d6928a9f36d8d4654a2ee34bd72aebfddf3
chander3012 <chanderprakash3012@gmail.com>
2025-10-22 13:50:40 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import Navbar from "./components/globalComponents/Navbar"
+
 import Page from "./pages/Landingpage/Page"
 export default function App() {
   return (
     <>
-    <Navbar />
     <Page/>
     </>
   )
--- src/pages/Landingpage/Page.jsx
+++ src/pages/Landingpage/Page.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import CustomerReviews from '../../components/Landingcomp/CustomerReviews'
 import TravelArticles from '../../components/Landingcomp/TravelArticles'
 import Newsletter from '../../components/Landingcomp/Newsletter'
+import Navbar from '../../components/globalComponents/Navbar'
 
 function Page() {
   return (
     <>
+    <Navbar/>
     <CustomerReviews/>
     <TravelArticles/>
     <Newsletter/>
